f5f20feb2a282b1293005f5a61ec9fe78dbbc197
Andrew <aribicki@gmail.com>
2015-02-18 15:21:02 -0500

    added code from class

--- Body.java
+++ Body.java
@@ -2,10 +2,28 @@
 // Code from page 42 
 // Class name Body
 
+// Additional code from book typed in by BLC 11 Feb 2015
+
 class Body {
 	public long idNum;
 	public String name;
 	public Body orbits;
 	
 	public static long nextID = 0;
+	public final double PI = 3.1416;
+/*------------------------------------------------------------------------*/
+	Body() { 
+ 	 idNum = nextID++;
+	 }
+	
+	Body(String bodyName, Body orbitsAround) {
+	 this();
+	 name = bodyName;
+	 orbits = orbitsAround;
+}
+	Body(String bodyName) {
+	 this();
+	 orbits = null;
+	 name = bodyName;
+	 }
 }
